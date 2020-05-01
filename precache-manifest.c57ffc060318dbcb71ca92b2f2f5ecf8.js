4edf9549333e0e24ae3f1a78712b7ada6633c5ce
Vladimir <79854322262@yandex.ru>
2020-05-01 21:15:11 +0300

    Updates

---
+++ precache-manifest.c57ffc060318dbcb71ca92b2f2f5ecf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994f88bcffdcbd207a7518ecfbfc7574",
+    "url": "/cross-zero/index.html"
+  },
+  {
+    "revision": "343d5d45996244652bda",
+    "url": "/cross-zero/static/css/main.f723170a.chunk.css"
+  },
+  {
+    "revision": "660708d59bf44faf1dc9",
+    "url": "/cross-zero/static/js/2.f0b011fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cross-zero/static/js/2.f0b011fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343d5d45996244652bda",
+    "url": "/cross-zero/static/js/main.46bb8307.chunk.js"
+  },
+  {
+    "revision": "97a19e394e8f51b11737",
+    "url": "/cross-zero/static/js/runtime-main.0514e9a0.js"
+  }
+]);
